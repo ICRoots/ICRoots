03e0b53b4898b285ae3048cb7e397e8029db46a6
icekidtech <edwinidopise@gmail.com>
2025-09-17 13:58:40 +0100

    fix: refactor Event type to a string and update service methods accordingly

--- src/declarations/event_bus_backend/event_bus_backend.did.d.ts
+++ src/declarations/event_bus_backend/event_bus_backend.did.d.ts
@@ -2,11 +2,10 @@ import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 import type { IDL } from '@dfinity/candid';
 
-export interface Event { 'message' : string, 'timestamp' : bigint }
+export type Event = string;
 export interface _SERVICE {
-  'emit' : ActorMethod<[string], undefined>,
-  'get_events' : ActorMethod<[], Array<Event>>,
-  'list_recent' : ActorMethod<[bigint], Array<string>>,
+  'emit' : ActorMethod<[Event], undefined>,
+  'list_recent' : ActorMethod<[bigint], Array<Event>>,
 }
 export declare const idlFactory: IDL.InterfaceFactory;
 export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
--- src/declarations/event_bus_backend/event_bus_backend.did.js
+++ src/declarations/event_bus_backend/event_bus_backend.did.js
@@ -1,9 +1,8 @@
 export const idlFactory = ({ IDL }) => {
-  const Event = IDL.Record({ 'message' : IDL.Text, 'timestamp' : IDL.Nat64 });
+  const Event = IDL.Text;
   return IDL.Service({
-    'emit' : IDL.Func([IDL.Text], [], []),
-    'get_events' : IDL.Func([], [IDL.Vec(Event)], ['query']),
-    'list_recent' : IDL.Func([IDL.Nat64], [IDL.Vec(IDL.Text)], ['query']),
+    'emit' : IDL.Func([Event], [], []),
+    'list_recent' : IDL.Func([IDL.Nat64], [IDL.Vec(Event)], ['query']),
   });
 };
 export const init = ({ IDL }) => { return []; };
